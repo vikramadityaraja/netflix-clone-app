a22203540e4bda79c59ee6e19ceaa652dd566472
Vikram <30330434+vikramadityaraja@users.noreply.github.com>
2023-01-24 18:32:18 +0530

    del

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 
+/*"homepage": "https://fireclint.github.io/netflix-react-tailwind/"*/
+
 const Navbar = () => {
   const { user, logOut } = UserAuth();
   const navigate = useNavigate();
